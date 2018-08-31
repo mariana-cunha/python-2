fbd92abd62a8d75e7300a6ddb3df383d8fee122c
mariana <mari.marques26@gmail.com>
2018-08-31 19:12:36 -0300

    aula 4

---
+++ biblioteca.py
@@ -0,0 +1,7 @@
+def gera_nome_convite(convite):
+    convite = 'mariana cunha'
+    posicao_final = len(convite)
+    posicao_inicial = posicao_final - 4
+    parte1 = convite[0:4]
+    parte2 = convite[posicao_inicial: posicao_final]
+    print ('%s %s' % (parte1, parte2))
